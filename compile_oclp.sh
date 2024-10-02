48fad10d3008e4a84458c40dacdd7d2a8c2ac16e
ali ghafourian <35195176+Edwardwich@users.noreply.github.com>
2024-10-02 04:44:25 +0330

    Update compile_oclp.sh

--- compile_oclp.sh
+++ compile_oclp.sh
@@ -53,20 +53,27 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Install the project (it may not require sudo, verify if necessary)
-bash install.sh
+# Run the installation of the privileged helper tool as sudo
+sudo bash install.sh
 cd ../..
+
+# Commented out the check for 'payloads.dmg'
+# Verify the presence of 'payloads.dmg' before running PyInstaller
+# if [ ! -f "payloads.dmg" ]; then
+#     echo "Error: 'payloads.dmg' not found, please ensure it exists in the project's root directory."
+#     exit 1
+# fi
 
 # Create and activate a virtual environment for Python dependencies
 python3 -m venv venv || { echo "Error: Failed to create virtual environment."; exit 1; }
 source venv/bin/activate || { echo "Error: Failed to activate virtual environment."; exit 1; }
 
 # Install Python dependencies using pip
-pip install -r requirements.txt || { echo "Error: Failed to install requirements."; exit 1; }
+pip3 install -r requirements.txt || { echo "Error: Failed to install requirements."; exit 1; }
 
-# Run the build command without sudo
-echo "Generating OpenCore-Patcher.app..."
-python3 -m PyInstaller ./OpenCore-Patcher-GUI.spec --noconfirm || { echo "Error: Failed to generate OpenCore-Patcher.app."; exit 1; }
+# Run PyInstaller to generate the application
+ echo "Generating OpenCore-Patcher.app..."
+ sudo ./Build-Project.command || { echo "Error: Build command failed."; exit 1; }
 
 # Run the OpenCore-Patcher GUI without sudo
 python3 ./OpenCore-Patcher-GUI.command || { echo "Error: OpenCore-Patcher GUI command failed."; exit 1; }
@@ -77,4 +84,3 @@ echo "Build complete."
 # Clean up
 deactivate  # Make sure to deactivate virtual environment at end
 exit 0
-
